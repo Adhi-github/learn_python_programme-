9b12bfdfaaddf57916fb2417caa248a122a0161c
Adhi-github <89089144+Adhi-github@users.noreply.github.com>
2021-08-18 11:31:46 +0530

    Add files via upload

---
+++ main (8).py
@@ -0,0 +1,43 @@
+#learn python programme 
+
+#Hello world 
+#print statement 
+'syntax of print statement is' 
+#print ()  #No print value added in print statement 
+
+#print (hi adhi)  #invalid syntax 
+
+#Because You Want declare value type 
+#Two type of print value Is There 
+#One Is string Another One Is number 
+'''string value Always represent '' or double ""
+   number Is represent like 5,66 '''
+
+#so,
+print ('hi adhi') #string type value 
+#Get output like hi adhi
+print ("Welcome adhi")  
+print (5)         #int type value 
+#display output like 5 
+
+'Also Do concatenation '
+#concatenation means arithmetic operation 
+#You Can Do string concatenation or int concatenation 
+'string concatenation '
+print ('hi'+'Bro')  #add The Two string value hiBro 
+'int concatenation'
+print (5+6)       # add Two int value like 11 
+
+'''print concatenation Not Work On 
+combination string to int 
+or 
+int to string '''
+#for example 
+#print (5+"Hello world ") #You got type of error
+#so change type value int to string 
+print ('5'+"Hello world") #Now You got 5Helloworld 
+
+
+
+
+
