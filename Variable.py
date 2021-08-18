cffb0560127d4bcec845ac91fb55d9c8e0537a8c
Adhi-github <89089144+Adhi-github@users.noreply.github.com>
2021-08-18 17:25:29 +0530

    Add files via upload

---
+++ Variable.py
@@ -0,0 +1,35 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+#variable 
+'''What Is variable ?
+ store The datatype value  '''
+ 
+'How The variable names declare  '
+#variable name Always declare
+#(A-Z or a-z ) character 
+#or start with (_)symbol character 
+#or combination of character and number 
+#variable name Is case sensitive 
+
+'for example '
+Variable_name = "adhi"  #string value  stored in variable_name
+print (Variable_name )  #print The stored value 
+_Var="Good"            
+print (_Var)            
+var1="hi"
+print (var1)
+
+'Not declare Variable name like This '
+#var&&='hi ' #You got invalid syntax 
+#1var ="kutty "
+
+'delete variable name '
+#use del keyword
+del var1  #delete var1 
+print (var1) #You got var1 Is Not defined 
