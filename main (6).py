c34cc1123ec4d0fe534937b2c510b6f1cf4b79a0
Adhi-github <89089144+Adhi-github@users.noreply.github.com>
2021-09-12 21:14:06 +0530

    Add files via upload

---
+++ main (6).py
@@ -0,0 +1,52 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+'You Can to This typecasting method '
+
+#typecasting means change The data type 
+'for example to Say :int to string (or)string to int  '
+#You can Do type casting Two way 
+'''1.directly change in variable 
+   2.or You Can do in print statement '''
+   
+#directly change 
+y=6  - Just comment It Other wise You got error 
+name ='kutty' 
+'Now You can change in direct variable type '
+y='6'
+print (name+y) #return kutty6 
+
+#or You can change direct print statement 
+print (str(name +y)) #return kutty6 
+
+'escalating function '
+#number of escalating parameters Are There '
+
+'''
+1.\n -return New line 
+2.\t -return 4 tab space 
+3.\\ -return \single backspace 
+4.\n\n -return Two line 
+5.\'-return single quotes 
+6.\"-return double quotes 
+'''
+#\n escalating 
+print ('hi \n Hello ')#return hi and Hello in Two separate line 
+
+#\t  escalating
+print ('hi\t hello ')#return hi     hello 
+
+#\' escalating 
+print ('hi\'Hello')#return hi'Hello 
+
+#Why ('' or "" )Are mentioned in print statement 
+
+'''*Because anything You written in print statement ,
+print consider That Is string , so That Why You Have to Had It  *'''
+   
+
