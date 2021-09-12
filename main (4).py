5e1d148128842178bdfac65ef5c43c7380e08c1a
Adhi-github <89089144+Adhi-github@users.noreply.github.com>
2021-09-12 21:09:56 +0530

    Add files via upload

---
+++ main (4).py
@@ -0,0 +1,38 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+#print statement 
+
+'syntax of print statement '
+print () #print Is empty so There Is No value print in output 
+
+'''If You Want print something in print statement so ,
+You could assign in print function '''
+
+'single word print statement '
+print ("Welcome to python world ") #You Could Always assign print statement in ("" or '')
+
+#multiple word print statement 
+print ("hi "'Hello'"Welcome")
+
+#type of function use in print statement 
+#1.sep =''  -separate The Word in print statement 
+#2.type ()  -type of value return in print statement 
+#3.end=""   -join to Two statement of print statement in One print statement
+'''escalating operation Also Do in print statement
+1.\n -return New line 
+2.\t-return 4 space 
+3.\\-return single backslash 
+4.\'-return single quote 
+5.\"-return double quotes 
+6.\n\n-return Two line''' 
+
+#4.+ -print concatenation 
+#5.If You anything return in print statement,print function consider It's string 
+#so You Could Always mention(print or int) What type of value You Want print 
+ 
