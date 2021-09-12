55ddcb7a16e522eca7365db8b5244fe20e286027
Adhi-github <89089144+Adhi-github@users.noreply.github.com>
2021-09-12 21:11:19 +0530

    Add files via upload

---
+++ main (5).py
@@ -0,0 +1,53 @@
+#print function 
+#1.seperate fuction 
+#2.type function
+#3.end function 
+#4.escalating function 
+#5.print concatenation
+
+'seperate function '
+#syntax of seperate Is 
+#sep=''
+print("Hello World",'Hello',sep=',')#return Hello world ,Hello 
+#If You want remove space 
+print ("hi",'good','to','see','You',sep='')#return higoodtoseeYou 
+'so You Can use number symbol in This separate function in If you Want '
+#You Can print website format Also
+print ('python','com',sep='.')#return python.com 
+
+'type() function '
+#syntax or type function Is 
+#type()
+name ='python'
+print(name ) #return name value 
+#If You Want name type value
+print (type(name ))#return name type 
+x=5
+print (type (x )) #rerun X type value 
+
+'end function '
+#multi line of statement print in One line 
+#syntax of end function Is 
+#end=''
+print ('print ',end ='')
+print ('text ') #return print This 
+#suppose If You Want add Some Word in between Two line 
+print ('print',end='This')
+print ('text') #return print This text 
+
+'print concatenation'
+#symbol use concatenation Is (+)
+#concatenation Work Only string to string (or)int to int combination 
+#Not Work on string to int or int to string 
+'for example '
+name ='kutty'
+name1='v'
+print (name +name1)#return kuttyv
+#suppose Try string to Int combination
+y=6
+print (name+y )#You got type value error 
+#so You Could change string value to int 
+'you Can Do This typecasting method  '
+
+
+ 
