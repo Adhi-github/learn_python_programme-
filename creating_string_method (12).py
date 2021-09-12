fc24745a720f17f2e5563d35ebbec87a49ed09fb
Adhi-github <89089144+Adhi-github@users.noreply.github.com>
2021-09-12 21:17:53 +0530

    Add files via upload

---
+++ creating_string_method (12).py
@@ -0,0 +1,36 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+#string methods in python
+
+#creating string 
+
+#single quote string method 
+
+name ='single quote printing method '
+print (name)
+
+#double quotes string method 
+
+name1 ="double quotes string method "
+print (name1)
+
+#triple quotes print method 
+
+name2 ='''Three
+quotes 
+method '''
+print (name2)
+#or print like This 
+name3 ='''triple string method '''
+print (name3 )
+
+
+
+
+
